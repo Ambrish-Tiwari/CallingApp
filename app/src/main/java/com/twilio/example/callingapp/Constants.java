47d03e5199af6918bbe84473df24bbde79f462b6
Ambrish <Ambrish>
2016-03-09 17:16:23 +0530

    Version 0.1 Calling from Twilio Number To Another Number Date: 9 March 2016

---
+++ app/src/main/java/com/twilio/example/callingapp/Constants.java
@@ -0,0 +1,10 @@
+package com.twilio.example.callingapp;
+
+/**
+ * Created by Ambrish on 3/9/2016.
+ */
+public class Constants {
+    public static final String CAPABILITY_TOKEN_URL = "http://callingapp.domain_name.com/AndroidApp/AndroidCapToken";
+    public static final String FROM_NUMBER  = "Enter your Twilio number"; // Twilio Number
+    public static final String TO_NUMBER  = "Enter the number you wont to call";
+}
